9978f07384b10d4eb9d4093a576c16a7ba5f5abf
Manutsawee <78074262+Manutsawee@users.noreply.github.com>
2023-12-30 22:47:27 +0800

    更新 dreadcraft_skins.lua
    add assets parameter

--- scripts/prefabs/dreadcraft_skins.lua
+++ scripts/prefabs/dreadcraft_skins.lua
@@ -5,6 +5,10 @@ table.insert(prefs, CreatPrefabSkin("night_edge",
     base_prefab = "dreadsword",
     type = "item",
 	rarity = "Elegant",
+    assets = {
+        Asset("DYNAMIC_ANIM", "anim/dynamic/night_edge.zip"),
+        Asset("PKGREF", "anim/dynamic/night_edge.dyn"),
+    },
     init_fn = function(inst) dreadsword_init_fn(inst, "night_edge") end,
 	skin_tags = { "DREADSWORD", "CRAFTABLE", },
 }
