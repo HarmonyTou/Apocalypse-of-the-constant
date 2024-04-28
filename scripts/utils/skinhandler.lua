641f7b1d944574948ac70407884bdb47cbfd5a82
Sydney <manutsawee.louis@outlook.com>
2024-04-28 12:56:23 +0800

    feat: skinhandler remove spaces

--- scripts/utils/skinhandler.lua
+++ scripts/utils/skinhandler.lua
@@ -300,18 +300,16 @@ local function set_rarity(rarity, order, color, override_symbol, override_build)
 end
 
 return {
-    --     GetModSkins                 = get_mod_skins,
-    --     AddModSkin                  = add_mod_skin,
-    --     RemoveModSkin               = remove_mod_skin,
-    --     IsModSkin                   = validate_mod_skin, -- Backward compatible
-    --     IsValidModSkin              = validate_mod_skin, -- Backward compatible
-    --     ValidateModSkin             = validate_mod_skin,
-
-    --     GetPlayerFromID             = get_player_from_id,
-    --     SetCharacterExclusiveSkin   = set_character_exlusive_skin,
-    --     DoesCharacterHaveSkin       = does_character_have_skin,
-    --     DoesCharacterHasSkin        = does_character_have_skin, -- Backward compatible
-
-    AddModSkins                 = add_mod_skins, -- Import skin data
-    -- SetRarity                   = set_rarity,
+    GetModSkins = get_mod_skins,
+    AddModSkin = add_mod_skin,
+    RemoveModSkin = remove_mod_skin,
+    IsModSkin = validate_mod_skin, -- Backward compatible
+    IsValidModSkin = validate_mod_skin, -- Backward compatible
+    ValidateModSkin = validate_mod_skin,
+    GetPlayerFromID = get_player_from_id,
+    SetCharacterExclusiveSkin = set_character_exlusive_skin,
+    DoesCharacterHaveSkin = does_character_have_skin,
+    DoesCharacterHasSkin = does_character_have_skin, -- Backward compatible
+    AddModSkins = add_mod_skins, -- Import skin data
+    SetRarity = set_rarity,
 }
