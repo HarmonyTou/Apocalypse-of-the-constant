9b30b1e1fa8216754bf47a4770baba3ed98fbc8e
Sydney <manutsawee.louis@outlook.com>
2024-04-28 12:57:37 +0800

    update: upvalueutil.lua

--- scripts/utils/upvalueutil.lua
+++ scripts/utils/upvalueutil.lua
@@ -1,32 +1,39 @@
-local function GetUpvalue(fn, name, recurse_levels)
-    assert(type(fn) == "function")
-
-    recurse_levels = recurse_levels or 0
-    local source_fn = fn
+---@param fn function
+---@param name string
+---@return any, number | nil
+local function get_upvalue(fn, name)
     local i = 1
-
     while true do
-        local _name, value = debug.getupvalue(fn, i)
-        if _name == nil then
+        local value_name, value = debug.getupvalue(fn, i)
+        if value_name == name then
+            return value, i
+        elseif value_name == nil then
             return
-        elseif _name == name then
-            return value, i, source_fn
-        elseif type(value) == "function" and recurse_levels > 0 then
-            local _value, _i, _source_fn = GetUpvalue(value, name, recurse_levels - 1)
-            if _value ~= nil then
-                return _value, _i, _source_fn
-            end
         end
-
         i = i + 1
     end
 end
 
-local function SetUpvalue(fn, value, name, recurse_levels)
-    local _, i, source_fn = GetUpvalue(fn, name, recurse_levels)
-    if source_fn and i and value then
-        debug.setupvalue(source_fn, i, value)
+---@param fn function
+---@param path string
+---@return any, number, function
+local function GetUpvalue(fn, path)
+    local value, prv, i = fn, nil, nil ---@type any function | nil, number | nil
+    for part in path:gmatch("[^%.]+") do
+        print(part)
+        prv = fn
+        value, i = get_upvalue(value, part)
+        assert(i ~= nil, "could't find " .. path .. " from: ", fn)
     end
+    return value, i, prv
+end
+
+---@param fn function
+---@param path string
+---@param value any
+local function SetUpvalue(fn, value, path)
+    local _, i, source_fn = ToolUtil.GetUpvalue(fn, path)
+    debug.setupvalue(source_fn, i, value)
 end
 
 return {
