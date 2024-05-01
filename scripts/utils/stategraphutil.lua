97d415c59a81be69007b63b15272b2fba8d9b160
Sydney <manutsawee.louis@outlook.com>
2024-05-01 15:23:14 +0800

    update stategraphutil.lua

--- scripts/utils/stategraphutil.lua
+++ scripts/utils/stategraphutil.lua
@@ -1,18 +1,21 @@
-local AddTimeEventPostInit = function(sg, name, index, fn, time)
+local AddTimeEventPostInit = function(sg, name, index, fn)
     if sg ~= nil then
         local _timeline = sg.states[name].timeline
         if _timeline ~= nil then
             local _timeevent_time = _timeline.time
             local _timeevent_fn = _timeline.fn
-            local _timeevent = TimeEvent(_timeevent_time, function(inst)
-                if fn ~= nil then
-                    fn(inst)
-                end
-                _timeevent_fn(inst)
-            end)
-
-            table.remove(_timeline, index)
-            table.insert(_timeline, index, _timeevent)
+            if _timeevent_time ~= nil then
+                local _timeevent = TimeEvent(_timeevent_time, function(inst)
+                    if fn ~= nil then
+                        fn(inst)
+                    end
+                    if _timeevent_fn ~= nil then
+                        _timeevent_fn(inst)
+                    end
+                end)
+                table.remove(_timeline, index)
+                table.insert(_timeline, index, _timeevent)
+            end
         end
     end
 end
