74bacbb17f5dfd59b69fa828ee9d805eba51ebc5
216bishop <168087371+216bishop@users.noreply.github.com>
2024-05-03 11:57:00 +0800

    Update assets.lua

--- main/assets.lua
+++ main/assets.lua
@@ -11,6 +11,7 @@ PrefabFiles = {
     "dread_cloak",
     "dread_axe",
     "aoc_skins",
+    "nightmare_hat"
 }
 
 Assets = {
@@ -18,7 +19,7 @@ Assets = {
     Asset("IMAGE", "images/apocalypse-of-the-constant.tex"),
     Asset("ATLAS", "images/apocalypse-of-the-constant.xml"),
     Asset("ATLAS_BUILD", "images/apocalypse-of-the-constant.xml", 256), -- for minisign
-    Asset("ANIM", "anim/obsidian_tool_meter.zip"),
+    Asset("ANIM", "lunar_spark_meter.zip"),
 }
 
 AssetUtil.RegisterImageAtlas("images/apocalypse-of-the-constant.xml")
